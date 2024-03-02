db33a13ac2669334fc9d432a5f25c6f614e04522
Jaimen789 <85990372+Jaimen789@users.noreply.github.com>
2024-03-02 10:46:49 +0200

    ✅ Login, Register

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+
+    firebase : {
+        apiKey: "AIzaSyDLy_R5nuXDolR7dMqCex6g4-QI7V5SSDU",
+        authDomain: "cos720-student-app.firebaseapp.com",
+        projectId: "cos720-student-app",
+        storageBucket: "cos720-student-app.appspot.com",
+        messagingSenderId: "693763850660",
+        appId: "1:693763850660:web:704a1b3dc68db7b8b04131"
+    }
+};
